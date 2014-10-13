1b27693016f48525b335624d9498b03b33e78661
Koichi Tsunoda <koichi@comr.se>
2014-10-13 00:12:20 -0700

    Base64-encoded "UUID" generator

---
+++ src/main/java/io/berkeley/hibernate/BIDIdentifierGenerator.java
@@ -0,0 +1,36 @@
+package io.berkeley.hibernate;
+
+
+import com.google.common.io.BaseEncoding;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@SuppressWarnings("UnusedDeclaration")
+public class BIDIdentifierGenerator
+        implements IdentifierGenerator {
+
+    //-------------------------------------------------------------
+    // Variables - Private - Static
+    //-------------------------------------------------------------
+
+    private static BaseEncoding BASE64_URL_ENCODING = BaseEncoding.base64Url().omitPadding();
+
+
+    //-------------------------------------------------------------
+    // Implementations - IdentifierGenerator
+    //-------------------------------------------------------------
+
+    public Serializable generate(SessionImplementor session, Object object)
+            throws HibernateException {
+        UUID uuid = UUID.randomUUID();
+
+        return BASE64_URL_ENCODING.encode(Bytes.concat(Longs.toByteArray(uuid.getMostSignificantBits()), Longs.toByteArray(uuid.getLeastSignificantBits()))).toLowerCase();
+    }
+}
